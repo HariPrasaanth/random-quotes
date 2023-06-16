49ac9f76caa23eb89c7210a28bff0a84a3525451
Hari Prasaanth <hariprasaanth1998@gmail.com>
2023-06-16 15:15:05 +0530

    Fetch URL changed

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
 
   const initialFetch = async () => {
     try {
-      let result = await fetch('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
+      let result = await fetch('https://raw.githubusercontent.com/HariPrasaanth/random-quotes/main/quotes.json')
       const data = await result.json()
       setQuotes(data.quotes)
       await generateRandomQuote(data.quotes, data.quotes.length)
